3278f541fe528cd783a32400b51ba9d1812917ed
Equinox4689 <qq_69745109@noreply.gitcode.com>
2025-05-29 01:12:02 +0800

    delete 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-from SceneGraph import SceneGraph
-with open('input/text.txt', 'r') as f:
-    text = f.read()
-
-scene_graph = SceneGraph(text, exist = True, exist_path = 'output/20250312193327')
-
-class_names = scene_graph.extract_objects_names()
-print(class_names)
